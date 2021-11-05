a1c562226da96d0b227be6b24a7769e4977b5240
akiyuki <akiyuki@akiyukis-MacBook-Pro.local>
2021-11-05 12:39:28 +0900

    add init

---
+++ setup_fish.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+sudo apt install -y fish peco
+
+fish -c "curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish"
+fish -c "fisher install jethrokuan/z oh-my-fish/theme-agnoster oh-my-fish/peco"
+mkdir ~/.config
+mkdir ~/.config/fish
+
+curl -L https://raw.githubusercontent.com/kakibara/fish_config/master/config.fish -o ~/.config/fish/config.fish
+curl -L https://raw.githubusercontent.com/kakibara/dev_env_docker/master/peco_select_history.fish -o ~/.config/fish/functions/peco_select_history.fish
