84489a106023b924b83f3b9615ccc2dc6e273d0d
sakakibara <sakakibara@kakipro.local>
2021-11-05 13:15:08 +0900

    pass mkdir

--- setup_fish.sh
+++ setup_fish.sh
@@ -4,8 +4,8 @@ sudo apt install -y fish peco
 
 fish -c "curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish"
 fish -c "fisher install jethrokuan/z oh-my-fish/theme-agnoster oh-my-fish/peco"
-mkdir ~/.config
-mkdir ~/.config/fish
+mkdir -p ~/.config
+mkdir -p ~/.config/fish
 
 curl -L https://raw.githubusercontent.com/kakibara/fish_config/main/files/config.fish -o ~/.config/fish/config.fish
 curl -L https://raw.githubusercontent.com/kakibara/fish_config/main/files/peco_select_history.fish -o ~/.config/fish/functions/peco_select_history.fish
